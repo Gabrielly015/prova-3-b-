a4092541f84694a0421d0620fa46e8e9b5591cd9
GabriellyMartins22C <143205738+Gabrielly015@users.noreply.github.com>
2024-09-05 18:32:40 -0300

    Add files via upload

---
+++ Prova (3°B).sql
@@ -0,0 +1,31 @@
+CREATE DATABASE prova_22c;
+USE prova_22c
+
+CREATE TABLE 
+nome_funcionario varchar(255)
+data_inicio varchar (255)
+id_funcionario varchar(255)
+
+CREATE TABLE 
+id_projeto varchar(255)
+id_funcionario varchar(255)
+data_inicio varchar(255)
+data_fim varchar(255)
+
+
+Liste o nome dos funcionários e as datas de início e termino dos projetos em que estão trabalhando
+
+SELECT id_funcionario,
+SELECT nome_funcionario, 
+FROM data_inicio, 
+iNNER JOIN data_fim;
+
+Exiba o nome dos funcionários que participaram de projetos que começaram após 01 de março de 2024
+
+SELECT nome_funcionario,
+FROM data_inicio,
+INNER JOIN data_fim;
+
+ 
+
+
